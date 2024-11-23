757418e822fea658c689a2f4cdf918d1ab72bbc3
babycheetahcrypto <babycheetacrypto@gmail.com>
2024-11-23 01:11:03 -0800

    786

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,8 +1,20 @@
-/** @type {import('postcss-load-config').Config} */
-const config = {
-  plugins: {
-    tailwindcss: {},
-  },
+// next.config.mjs
+/** @type {import('next').NextConfig} */
+import * as _Sentry from "@sentry/nextjs";
+
+const nextConfig = {
+  reactStrictMode: true,
+  // Add other configurations as needed
 };
 
-export default config;
+export default _Sentry.withSentryConfig(nextConfig, {
+  // Sentry configuration options
+  sentry: {
+    hideSourceMaps: true,
+    disableServerWebpackPlugin: true,
+    disableClientWebpackPlugin: true,
+  },
+  // Optional: Manually configure source map uploading
+  org: process.env.SENTRY_ORG,
+  project: process.env.SENTRY_PROJECT,
+});
