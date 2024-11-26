0664eb5020a8b7c77d73d685d47a6d85bf2e44b2
babycheetahcrypto <babycheetacrypto@gmail.com>
2024-11-26 11:09:57 -0800

    777

--- middleware.ts
+++ middleware.ts
@@ -2,15 +2,9 @@ import { NextResponse } from 'next/server';
 import type { NextRequest } from 'next/server';
 
 export function middleware(request: NextRequest) {
-  const telegram = request.headers.get('user-agent')?.includes('TelegramWebApp');
-
-  if (!telegram && process.env.NODE_ENV === 'production') {
-    return NextResponse.redirect(new URL('https://t.me/BabyCheetah_Bot'));
-  }
-
   return NextResponse.next();
 }
 
 export const config = {
-  matcher: '/:path*',
+  matcher: ['/((?!api|_next/static|_next/image|favicon.ico).*)'],
 };
