bee82b627229d5592c79e03531d47577b3418a4f
babycheetahcrypto <babycheetacrypto@gmail.com>
2024-12-01 05:15:50 -0800

    777

--- app/api/user.ts
+++ app/api/user.ts
@@ -70,8 +70,6 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
         where: { telegramId: parseInt(telegramId) },
         update: {
           username,
-          firstName,
-          lastName,
           coins,
           level,
           exp,
