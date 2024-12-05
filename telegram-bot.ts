d6db3e1e2e6d60fb3aeddadcef7609acb315a8a7
babycheetahcrypto <babycheetacrypto@gmail.com>
2024-12-05 08:15:25 -0800

    777

--- telegram-bot.ts
+++ telegram-bot.ts
@@ -14,14 +14,15 @@ bot.command('start', async (ctx: Context) => {
   const welcomeMessage = `
 Welcome to Baby Cheetah! 🎉🎉🎉
 
-At Baby Cheetah, we’re redefining 
-crypto gaming with exciting tap-to-earn
- mechanics, social engagement, and 
-exclusive rewards. Collect Baby Cheetah 
-Coins $BBCH, complete tasks, and prepare
-for something big—an airdrop is coming soon! 🚀💸
+At Baby Cheetah, we’re redefining crypto 
+gaming with exciting tap-to-earn mechanics,
+social engagement, and exclusive rewards. 
+Collect Baby Cheetah Coins $BBCH, complete 
+tasks, and preparefor something big—an 
+airdrop is coming soon! 🚀💸
 
-Here’s what you can do with Baby Cheetah now:
+🥊 Here’s what you can do with Baby
+Cheetah 🐾 now:
 💰 Earn Baby Cheetah Coins: Tap, play, and 
 collect $BBCH in our fun and Mining game.
 👥 Invite Friends: Share the game 
