ed5861f5cd25a28a008de87942ca57ad2c280ff4
babycheetahcrypto <babycheetacrypto@gmail.com>
2024-12-06 04:27:31 -0800

    777

--- components/crypto-game.tsx
+++ components/crypto-game.tsx
@@ -1636,9 +1636,9 @@ const CryptoGame: React.FC<CryptoGameProps> = ({ userData, onCoinsUpdate, saveUs
           </div>
         </div>
 
-        <div className="flex flex-col items-center justify-center w-full max-w-[300] mx-auto mb-16">
+        <div className="flex flex-col items-center justify-center w-full max-w-[300] mx-auto mb-6">
           <button
-            className="w-[calc(100vw-8px)] max-w-[500px] aspect-square rounded-full overflow-hidden shadow-lg z-20 coin-button mb-6 transform transition-all duration-100 active:scale-95 hover:shadow-2xl hover:scale-105"
+            className="w-[calc(100vw-8px)] max-w-[300px] aspect-square rounded-full overflow-hidden shadow-lg z-20 coin-button mb-6 transform transition-all duration-100 active:scale-95 hover:shadow-2xl hover:scale-105"
             onClick={clickCoin}
             onTouchStart={(e) => {
               e.preventDefault();
