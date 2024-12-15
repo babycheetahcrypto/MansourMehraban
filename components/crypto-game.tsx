bf28fe5ab593ca8271b207636067a2b06de8c9c1
babycheetahcrypto <babycheetacrypto@gmail.com>
2024-12-15 03:46:54 -0800

    777

--- components/crypto-game.tsx
+++ components/crypto-game.tsx
@@ -497,7 +497,7 @@ const CryptoGame: React.FC<CryptoGameProps> = ({ userData, onCoinsUpdate, saveUs
       username: '',
       firstName: '',
       lastName: '',
-      coins: 0,
+      coins: 4990,
       level: 1,
       exp: 0,
       profilePhoto: '',
