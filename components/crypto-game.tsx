9c2494306dd3da3b685ff976d8e37d8e0fcc08b1
babycheetahcrypto <babycheetacrypto@gmail.com>
2024-12-20 16:02:58 -0800

    777

--- components/crypto-game.tsx
+++ components/crypto-game.tsx
@@ -2077,13 +2077,13 @@ const CryptoGame: React.FC<CryptoGameProps> = ({ userData, onCoinsUpdate, saveUs
                   <h3 className="text-sm font-bold text-center mb-2 group-hover:text-primary transition-colors duration-300 text-white">
                     {item.name}
                   </h3>
-                  <div className="relative w-full h-24 mb-2 overflow-hidden rounded-[10px] group-hover:scale-105 transition-transform duration-300">
+                  <div className="relative w-full h-24 mb-2 overflow-hidden rounded-[15px] group-hover:scale-105 transition-transform duration-300">
                     <Image
                       src={item.image}
                       alt={item.name}
                       layout="fill"
-                      objectFit="cover rounded-[10px]"
-                      className={`relative z-10 ${
+                      objectFit="contain"
+                      className={`relative z-10 rounded-[15px] ${
                         !unlockedLevels.includes(index + 1)
                           ? 'group-hover:opacity-80 transition-opacity duration-300'
                           : ''
@@ -2144,13 +2144,13 @@ const CryptoGame: React.FC<CryptoGameProps> = ({ userData, onCoinsUpdate, saveUs
                   <h3 className="text-sm font-bold text-center mb-2 group-hover:text-primary transition-colors duration-300 text-white">
                     {item.name}
                   </h3>
-                  <div className="relative w-full h-24 mb-2 overflow-hidden rounded-[10px] group-hover:scale-105 transition-transform duration-300">
+                  <div className="relative w-full h-24 mb-2 overflow-hidden rounded-[15px] group-hover:scale-105 transition-transform duration-300">
                     <Image
                       src={item.image}
                       alt={item.name}
                       layout="fill"
-                      objectFit="cover rounded-[10px]"
-                      className="group-hover:opacity-80 transition-opacity duration-300"
+                      objectFit="contain"
+                      className="rounded-[15px] group-hover:opacity-80 transition-opacity duration-300"
                       draggable="false"
                       onContextMenu={(e) => e.preventDefault()}
                     />
