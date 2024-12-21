51b71d67eebd2e55bfd11758321e14431327073e
babycheetahcrypto <babycheetacrypto@gmail.com>
2024-12-21 05:36:01 -0800

    777

--- utils/formatNumber.ts
+++ utils/formatNumber.ts
@@ -1,13 +1,9 @@
-const formatNumberWithSuffix = (num: number): string => {
-  if (num >= 1e9) return Math.floor(num / 1e9).toFixed(1) + 'b';
-  if (num >= 1e6) return Math.floor(num / 1e6).toFixed(1) + 'm';
-  if (num >= 1e3) return Math.floor(num / 1e3).toFixed(1) + 'k';
-  return Math.floor(num).toString();
-};
-
-const formatNumber = (num: number, useShortFormat: boolean = true): string => {
+export function formatNumber(num: number, useShortFormat: boolean = true): string {
   if (useShortFormat) {
-    return formatNumberWithSuffix(Math.floor(num));
+    if (num >= 1e9) return (num / 1e9).toFixed(1) + 'b';
+    if (num >= 1e6) return (num / 1e6).toFixed(1) + 'm';
+    if (num >= 1e3) return (num / 1e3).toFixed(1) + 'k';
+    return num.toString();
   }
-  return Math.floor(num).toLocaleString('en-US');
-};
+  return num.toLocaleString('en-US');
+}
