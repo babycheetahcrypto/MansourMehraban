1991c2277d980234034d41db158ea2b1646e6127
babycheetahcrypto <babycheetacrypto@gmail.com>
2024-12-23 08:06:52 -0800

    777

--- components/wallet.tsx
+++ components/wallet.tsx
@@ -157,7 +157,7 @@ const Wallet: React.FC<WalletProps> = ({ coins, onWalletConnect }) => {
                   alt="Airdrop"
                   width={150}
                   height={150}
-                  className="relative z-10 transition-all duration-300 transform hover:scale-110"
+                  className="relative z-10 rounded-full bg-gradient-to-r from-purple-400 via-pink-500 to-red-500 blur-md"
                   draggable="false"
                   onContextMenu={(e) => e.preventDefault()}
                 />
@@ -178,7 +178,7 @@ const Wallet: React.FC<WalletProps> = ({ coins, onWalletConnect }) => {
                   draggable="false"
                   onContextMenu={(e) => e.preventDefault()}
                 />
-                <p className="text-2xl font-extrabold text-green-400 animate-pulse">
+                <p className="text-2xl font-extrabold text-green-400">
                   {formatNumber(coins, false)}
                 </p>
               </div>
