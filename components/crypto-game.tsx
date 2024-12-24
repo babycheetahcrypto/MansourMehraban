ca8388e9368fd97ba5f1ffc77319c8964a0719b9
babycheetahcrypto <babycheetacrypto@gmail.com>
2024-12-24 09:07:05 -0800

    777

--- components/crypto-game.tsx
+++ components/crypto-game.tsx
@@ -1898,7 +1898,7 @@ const CryptoGame: React.FC<CryptoGameProps> = ({ userData, onCoinsUpdate, saveUs
             </div>
             <div>
               <h2 className="font-black text-base text-white">
-                {`${user.firstName || ''} ${user.lastName || ''}`.trim().slice(0, 10) + '...'}
+                {`${user.firstName || ''} ${user.lastName || ''}`.trim().slice(0, 12)}
               </h2>
               <div className="text-sm text-white font-bold flex items-center">
                 <Image
