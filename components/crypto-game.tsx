ca11c78c51353a641dfb09e3204360f78cc15fd6
babycheetahcrypto <babycheetacrypto@gmail.com>
2024-12-25 08:14:42 -0800

    777

--- components/crypto-game.tsx
+++ components/crypto-game.tsx
@@ -1851,7 +1851,7 @@ const CryptoGame: React.FC<CryptoGameProps> = ({ userData, onCoinsUpdate, saveUs
 
 
   const renderHome = () => (
-    <div className="flex-grow flex flex-col items-center justify-between p-4 relative overflow-hidden">
+    <div className="flex flex-col items-center justify-between h-full p-4 relative overflow-hidden">
       <div className="w-full max-w-md flex flex-col justify-between h-full">
         <div>
           <div className="flex space-x-2 mb-4 w-full">
@@ -1962,7 +1962,7 @@ const CryptoGame: React.FC<CryptoGameProps> = ({ userData, onCoinsUpdate, saveUs
           </div>
         </div>
 
-        <div className="flex-grow flex flex-col items-center justify-center">
+        <div className="flex-grow flex items-center justify-center">
           <button
             className="w-[350px] h-[350px] rounded-full overflow-hidden shadow-lg z-20 coin-button mb-4 relative bg-transparent"
             onClick={clickCoin}
@@ -1986,7 +1986,7 @@ const CryptoGame: React.FC<CryptoGameProps> = ({ userData, onCoinsUpdate, saveUs
           </button>
         </div>
 
-        <div className="w-full mb-20"> {/* Increased bottom margin */}
+        <div className="w-full mt-4"> {/* Increased bottom margin */}
           <div className="w-full mb-2">
             <div className="flex justify-between text-sm mb-2 text-white font-bold">
               <span className="font-bold flex items-center gap-2">
