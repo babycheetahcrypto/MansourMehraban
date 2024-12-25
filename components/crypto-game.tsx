07ffada7350dd91e1d7a18826a184076a1201e0f
babycheetahcrypto <babycheetacrypto@gmail.com>
2024-12-25 09:18:18 -0800

    777

--- components/crypto-game.tsx
+++ components/crypto-game.tsx
@@ -3418,7 +3418,7 @@ const CryptoGame: React.FC<CryptoGameProps> = ({ userData, onCoinsUpdate, saveUs
         <div
           className="flex-grow pb-24"
           style={{
-            paddingBottom: '1)',
+            paddingBottom: '0',
             marginBottom: '0',
             marginTop: '0', // Remove any top margin
           }}
