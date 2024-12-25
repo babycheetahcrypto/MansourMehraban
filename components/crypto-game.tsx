530912f231c58ba59e28e7c52877e30daa340a0f
babycheetahcrypto <babycheetacrypto@gmail.com>
2024-12-25 10:30:10 -0800

    777

--- components/crypto-game.tsx
+++ components/crypto-game.tsx
@@ -1852,7 +1852,7 @@ const CryptoGame: React.FC<CryptoGameProps> = ({ userData, onCoinsUpdate, saveUs
 
   const renderHome = () => (
     <div className="flex flex-col items-center justify-between h-full p-4 relative overflow-hidden">
-      <div className="w-full max-w-md flex flex-col justify-between h-full">
+      <div className="w-full max-w-md flex flex-col justify-between h-full pb-16">
         <div>
           <div className="flex space-x-2 mb-1 w-full"> {/* Reduced margin-bottom */}
             <div className="flex-1 bg-gradient-to-r from-gray-800/50 to-gray-900/50 rounded-xl p-2 backdrop-blur-md flex flex-col justify-center">
@@ -1962,7 +1962,7 @@ const CryptoGame: React.FC<CryptoGameProps> = ({ userData, onCoinsUpdate, saveUs
           </div>
         </div>
   
-        <div className="flex-grow flex items-center justify-center">
+        <div className="flex items-center justify-center my-4">
           <button
             className="w-[350px] h-[350px] rounded-full overflow-hidden shadow-lg z-20 coin-button mb-4 relative bg-transparent"
             onClick={clickCoin}
@@ -1986,8 +1986,8 @@ const CryptoGame: React.FC<CryptoGameProps> = ({ userData, onCoinsUpdate, saveUs
           </button>
         </div>
   
-        <div className="w-full mb-1"> {/* Reduced bottom margin */}
-          <div className="w-full mb-1">
+        <div className="w-full"> {/* Reduced bottom margin */}
+          <div className="w-full">
             <div className="flex justify-between text-sm mb-2 text-white font-bold">
               <span className="font-bold flex items-center gap-2">
                 <Image
