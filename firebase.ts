41d8a6792750c4d19fc18579f5d7c18b6d9bd5cc
babycheetahcrypto <babycheetacrypto@gmail.com>
2025-01-02 04:34:28 -0800

    777

--- firebase.ts
+++ firebase.ts
@@ -2,12 +2,14 @@ import { initializeApp, getApps } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  apiKey: "AIzaSyDVOXhGk_Zr1KyR2fTveWFu4D6LX_8hfRQ",
+  authDomain: "baby-cheetah.firebaseapp.com",
+  databaseURL: "https://baby-cheetah-default-rtdb.firebaseio.com",
+  projectId: "baby-cheetah",
+  storageBucket: "baby-cheetah.firebasestorage.app",
+  messagingSenderId: "273145423682",
+  appId: "1:273145423682:web:e32a4a92ae70f456164adb",
+  measurementId: "G-WKV5C9006C"
 };
 
 // Initialize Firebase
