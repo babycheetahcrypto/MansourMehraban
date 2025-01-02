272da470ffdf0489a76a9aee6f69f61b616a0680
babycheetahcrypto <babycheetacrypto@gmail.com>
2025-01-02 04:01:03 -0800

    777

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ const GameClient = dynamic(() => import('./game-client'), { ssr: false });
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main>
       <GameClient />
     </main>
   );
